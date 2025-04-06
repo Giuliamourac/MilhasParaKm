e962119b5eb83a07929376bbba368a553ca5150d
Giuliamourac <giuliamoura.carnevale@gmail.com>
2025-04-06 11:03:33 -0300

    finaliza programa

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MilhasPraKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double milhas, km;
+
+            Console.WriteLine("Digite uma medida em milhas para ser transformada em Km.");
+            milhas = Convert.ToDouble(Console.ReadLine()!);
+
+            km = milhas * 1.609;
+            Console.WriteLine($"{km} km.");
+        }
+    }
+}
